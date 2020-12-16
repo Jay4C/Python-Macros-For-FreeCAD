9f896ca96e1afa19c41e06390416d357f7a8948b
Jason ALOYAU <jason.aloyau@outlook.fr>
2020-12-16 18:49:15 +0100

    https://wiki.freecadweb.org/Scripted_Parts:_Ball_Bearing_-_Part_1

---
+++ Miscellaneous/ball_bearing_part_1.py
@@ -0,0 +1,85 @@
+# https://wiki.freecadweb.org/Scripting_examples
+# https://wiki.freecadweb.org/Scripted_Parts:_Ball_Bearing_-_Part_1
+## Ball-bearing script
+## 11.08.2016 by r-frank (BPLRFE/LearnFreeCAD on Youtube)
+## based on ball bearing script by JMG
+## (http://linuxforanengineer.blogspot.de/2013/08/free-cad-bearing-script.html)
+#
+#needed for inserting primitives
+import Part
+#needed for calculating the positions of the balls
+import math
+#needed for translation of torus
+from FreeCAD import Base
+#
+#VALUES#
+#(radius of shaft/inner radius of inner ring)
+R1=15.0
+#(outer radius of inner ring)
+R2=25.0
+#(inner radius of outer ring)
+R3=30.0
+#(outer radius of outer ring)
+R4=40.0
+#(thickness of bearing)
+TH=15.0
+#(number of balls)
+NBall=10
+#(radius of ball)
+RBall=5.0
+#(rounding radius for fillets)
+RR=1
+#first coordinate of center of ball
+CBall=((R3-R2)/2)+R2
+#second coordinate of center of ball
+PBall=TH/2
+
+#
+#Create new document
+App.newDocument("Unnamed")
+App.setActiveDocument("Unnamed")
+App.ActiveDocument=App.getDocument("Unnamed")
+Gui.ActiveDocument=Gui.getDocument("Unnamed")
+
+#
+#Inner Ring#
+B1=Part.makeCylinder(R1,TH)
+B2=Part.makeCylinder(R2,TH)
+IR=B2.cut(B1)
+#get edges and apply fillets
+Bedges=IR.Edges
+IRF=IR.makeFillet(RR,Bedges)
+#create groove and show shape
+T1=Part.makeTorus(CBall,RBall)
+T1.translate(Base.Vector(0,0,TH/2))
+InnerRing=IRF.cut(T1)
+Part.show(InnerRing)
+
+#
+#Outer Ring#
+B3=Part.makeCylinder(R3,TH)
+B4=Part.makeCylinder(R4,TH)
+OR=B4.cut(B3)
+#get edges and apply fillets
+Bedges=OR.Edges
+ORF=OR.makeFillet(RR,Bedges)
+#create groove and show shape
+T2=Part.makeTorus(CBall,RBall)
+T2.translate(Base.Vector(0,0,TH/2))
+OuterRing=ORF.cut(T2)
+Part.show(OuterRing)
+
+#
+#Balls#
+for i in range(NBall):
+  Ball=Part.makeSphere(RBall)
+  Alpha=(i*2*math.pi)/NBall
+  BV=(CBall*math.cos(Alpha),CBall*math.sin(Alpha),TH/2)
+  Ball.translate(BV)
+  Part.show(Ball)
+
+#
+#Make it pretty#
+App.activeDocument().recompute()
+Gui.activeDocument().activeView().viewAxometric()
+Gui.SendMsgToActiveView("ViewFit")
