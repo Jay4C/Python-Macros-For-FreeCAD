71a1eaea309cb521caa75c63d6505d402fc1eab1
Jason ALOYAU <jason.aloyau@outlook.fr>
2020-12-17 14:06:18 +0100

    https://wiki.freecadweb.org/Python

---
+++ Miscellaneous/python.py
@@ -0,0 +1,51 @@
+# https://wiki.freecadweb.org/Scripting_examples
+# https://wiki.freecadweb.org/Python
+
+Wire = make_wire(pointslist, closed=False, placement=None, face=None, support=None)
+
+Wire = make_wire(pointslist, False, None, None, None)
+Wire = make_wire(pointslist, False, None, None)
+Wire = make_wire(pointslist, False, None)
+Wire = make_wire(pointslist, False)
+Wire = make_wire(pointslist)
+
+Wire = make_wire(pointslist, closed=False, placement=None, face=None)
+Wire = make_wire(pointslist, closed=False, face=None, placement=None)
+Wire = make_wire(pointslist, placement=None, closed=False, face=None)
+Wire = make_wire(pointslist, support=None, closed=False, placement=None, face=None)
+
+p1 = Vector(0, 0, 0)
+p2 = Vector(1, 1, 0)
+p3 = Vector(2, 0, 0)
+Wire = make_wire([p1, p2, p3], closed=True)
+
+a_list = [1, 2, 3,
+          2, 4, 5]
+
+Wire = make_wire(pointslist,
+                False, None,
+                None, None)
+
+Wire = make_wire(pointslist, closed=True, face=True)
+Window = make_window(Wire, name="Big window")
+
+import FreeCAD as App
+import Draft
+
+p1 = App.Vector(0, 0, 0)
+p2 = App.Vector(1, 1, 0)
+p3 = App.Vector(2, 0, 0)
+Wire = Draft.make_wire([p1, p2, p3], closed=True)
+
+import FreeCAD as App
+import Draft
+import Arch
+
+p1 = App.Vector(0, 0, 0)
+p2 = App.Vector(1, 0, 0)
+p3 = App.Vector(1, 1, 0)
+p4 = App.Vector(0, 2, 0)
+pointslist = [p1, p2, p3, p4]
+
+Wire = Draft.make_wire(pointslist, closed=True, face=True)
+Structure = Arch.make_structure(Wire, name="Big pillar")
