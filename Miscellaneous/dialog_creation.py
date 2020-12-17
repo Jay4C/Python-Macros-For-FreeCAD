6270cda57ac1eed269cf05aeda36420c2ae1d80f
Jason ALOYAU <jason.aloyau@outlook.fr>
2020-12-17 13:16:39 +0100

    https://wiki.freecadweb.org/Dialog_creation

---
+++ Miscellaneous/dialog_creation.py
@@ -0,0 +1,71 @@
+# https://wiki.freecadweb.org/Scripting_examples
+# https://wiki.freecadweb.org/Dialog_creation
+
+# Form implementation generated from reading ui file 'mywidget.ui'
+#
+# Created: Mon Jun  1 19:09:10 2009
+#      by: PyQt4 UI code generator 4.4.4
+# Modified for PySide 16:02:2015 
+# WARNING! All changes made in this file will be lost!
+
+from PySide import QtCore, QtGui
+import FreeCAD, Part 
+
+class Ui_Dialog(object):
+   def setupUi(self, Dialog):
+       Dialog.setObjectName("Dialog")
+       Dialog.resize(187, 178)
+       self.title = QtGui.QLabel(Dialog)
+       self.title.setGeometry(QtCore.QRect(10, 10, 271, 16))
+       self.title.setObjectName("title")
+       self.label_width = QtGui.QLabel(Dialog)
+       self.label_width.setGeometry(QtCore.QRect(10, 50, 57, 16))
+       self.label_width.setObjectName("label_width")
+       self.label_height = QtGui.QLabel(Dialog)
+       self.label_height.setGeometry(QtCore.QRect(10, 90, 57, 16))
+       self.label_height.setObjectName("label_height")
+       self.width = QtGui.QLineEdit(Dialog)
+       self.width.setGeometry(QtCore.QRect(60, 40, 111, 26))
+       self.width.setObjectName("width")
+       self.height = QtGui.QLineEdit(Dialog)
+       self.height.setGeometry(QtCore.QRect(60, 80, 111, 26))
+       self.height.setObjectName("height")
+       self.create = QtGui.QPushButton(Dialog)
+       self.create.setGeometry(QtCore.QRect(50, 140, 83, 26))
+       self.create.setObjectName("create")
+
+       self.retranslateUi(Dialog)
+       QtCore.QObject.connect(self.create,QtCore.SIGNAL("pressed()"),self.createPlane)
+       QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+   def retranslateUi(self, Dialog):
+       Dialog.setWindowTitle("Dialog")
+       self.title.setText("Plane-O-Matic")
+       self.label_width.setText("Width")
+       self.label_height.setText("Height")
+       self.create.setText("Create!")
+       print("tyty")
+   def createPlane(self):
+       try:
+           # first we check if valid numbers have been entered
+           w = float(self.width.text())
+           h = float(self.height.text())
+       except ValueError:
+           print("Error! Width and Height values must be valid numbers!")
+       else:
+           # create a face from 4 points
+           p1 = FreeCAD.Vector(0,0,0)
+           p2 = FreeCAD.Vector(w,0,0)
+           p3 = FreeCAD.Vector(w,h,0)
+           p4 = FreeCAD.Vector(0,h,0)
+           pointslist = [p1,p2,p3,p4,p1]
+           mywire = Part.makePolygon(pointslist)
+           myface = Part.Face(mywire)
+           Part.show(myface)
+
+class plane():
+  def __init__(self):
+      self.d = QtGui.QWidget()
+      self.ui = Ui_Dialog()
+      self.ui.setupUi(self.d)
+      self.d.show()
