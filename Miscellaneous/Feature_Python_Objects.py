7245e1ecf67adacc37c031466dee4f56a826d070
Jason ALOYAU <jason.aloyau@outlook.fr>
2020-12-17 11:55:10 +0100

    https://wiki.freecadweb.org/FeaturePython_Objects

---
+++ Miscellaneous/Feature_Python_Objects.py
@@ -0,0 +1,40 @@
+# https://wiki.freecadweb.org/Scripting_examples
+# https://wiki.freecadweb.org/FeaturePython_Objects
+
+import FreeCAD as App
+
+def create(obj_name):
+    """
+    Object creation method
+    """
+
+    obj = App.ActiveDocument.addObject('App::FeaturePython', obj_name)
+
+    box(obj)
+
+    App.ActiveDocument.recompute()
+
+    return obj
+
+class box():
+
+    def __init__(self, obj):
+        """
+        Default constructor
+        """
+
+        self.Type = 'box'
+
+        obj.Proxy = self
+
+        obj.addProperty('App::PropertyString', 'Description', 'Base', 'Box description').Description = ""
+        obj.addProperty('App::PropertyLength', 'Length', 'Dimensions', 'Box length').Length = 10.0
+        obj.addProperty('App::PropertyLength', 'Width', 'Dimensions', 'Box width').Width = '10 mm'
+        obj.addProperty('App::PropertyLength', 'Height', 'Dimensions', 'Box height').Height = '1 cm'
+
+    def execute(self, obj):
+        """
+        Called on document recompute
+        """
+
+        print('Recomputing {0:s} ({1:s})'.format(obj.Name, self.Type))
