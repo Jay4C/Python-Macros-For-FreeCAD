968e3d83bdd6bd34625e7b6224634f32c970bd44
Jason ALOYAU <jason.aloyau@outlook.fr>
2020-12-17 13:57:36 +0100

    https://wiki.freecadweb.org/Extra_python_modules

---
+++ Miscellaneous/extra_python_modules.py
@@ -0,0 +1,97 @@
+# https://wiki.freecadweb.org/Scripting_examples
+# https://wiki.freecadweb.org/Extra_python_modules
+
+import PySide
+from PySide import QtCore,QtGui
+FreeCADWindow = FreeCADGui.getMainWindow()
+
+FreeCADWindow.addDockWidget(QtCore.Qt.RghtDockWidgetArea,my_custom_widget)
+
+text = text.encode('utf-8')
+
+path = FreeCAD.ConfigGet("AppHomePath")
+#path = FreeCAD.ConfigGet("UserAppData")
+OpenName, Filter = PySide.QtGui.QFileDialog.getOpenFileName(None, "Read a txt file", path, "*.txt")
+
+path = FreeCAD.ConfigGet("AppHomePath")
+#path = FreeCAD.ConfigGet("UserAppData")
+SaveName, Filter = PySide.QtGui.QFileDialog.getSaveFileName(None, "Save a file txt", path, "*.txt")
+
+try:
+    import PyQt4                                        # PyQt4
+    from PyQt4 import QtGui ,QtCore                     # PyQt4
+    from PyQt4.QtGui import QComboBox                   # PyQt4
+    from PyQt4.QtGui import QMessageBox                 # PyQt4
+    from PyQt4.QtGui import QTableWidget, QApplication  # PyQt4
+    from PyQt4.QtGui import *                           # PyQt4
+    from PyQt4.QtCore import *                          # PyQt4
+except Exception:
+    import PySide                                       # PySide
+    from PySide import QtGui ,QtCore                    # PySide
+    from PySide.QtGui import QComboBox                  # PySide
+    from PySide.QtGui import QMessageBox                # PySide
+    from PySide.QtGui import QTableWidget, QApplication # PySide
+    from PySide.QtGui import *                          # PySide
+    from PySide.QtCore import *                         # PySide
+
+myNewFreeCADWidget = QtGui.QDockWidget()          # create a new dockwidget
+myNewFreeCADWidget.ui = Ui_MainWindow()           # myWidget_Ui()             # load the Ui script
+myNewFreeCADWidget.ui.setupUi(myNewFreeCADWidget) # setup the ui
+try:
+    app = QtGui.qApp                              # PyQt4 # the active qt window, = the freecad window since we are inside it
+    FCmw = app.activeWindow()                     # PyQt4 # the active qt window, = the freecad window since we are inside it
+    FCmw.addDockWidget(QtCore.Qt.RightDockWidgetArea,myNewFreeCADWidget) # add the widget to the main window
+except Exception:
+    FCmw = FreeCADGui.getMainWindow()             # PySide # the active qt window, = the freecad window since we are inside it 
+    FCmw.addDockWidget(QtCore.Qt.RightDockWidgetArea,myNewFreeCADWidget) # add the widget to the main window
+
+try:
+    text = unicode(text, 'ISO-8859-1').encode('UTF-8')  # PyQt4
+except Exception:
+    text = text.encode('utf-8')                         # PySide
+
+OpenName = ""
+try:
+    OpenName = QFileDialog.getOpenFileName(None,QString.fromLocal8Bit("Lire un fichier FCInfo ou txt"),path,"*.FCInfo *.txt") # PyQt4
+except Exception:
+    OpenName, Filter = PySide.QtGui.QFileDialog.getOpenFileName(None, "Lire un fichier FCInfo ou txt", path, "*.FCInfo *.txt")#PySide
+
+SaveName = ""
+try:
+    SaveName = QFileDialog.getSaveFileName(None,QString.fromLocal8Bit("Sauver un fichier FCInfo"),path,"*.FCInfo") # PyQt4
+except Exception:
+    SaveName, Filter = PySide.QtGui.QFileDialog.getSaveFileName(None, "Sauver un fichier FCInfo", path, "*.FCInfo")# PySide
+
+def errorDialog(msg):
+    diag = QtGui.QMessageBox(QtGui.QMessageBox.Critical,u"Error Message",msg )
+    try:
+        diag.setWindowFlags(PyQt4.QtCore.Qt.WindowStaysOnTopHint) # PyQt4 # this function sets the window before
+    except Exception:    
+        diag.setWindowFlags(PySide.QtCore.Qt.WindowStaysOnTopHint)# PySide # this function sets the window before
+#    diag.setWindowModality(QtCore.Qt.ApplicationModal)       # function has been disabled to promote "WindowStaysOnTopHint"
+    diag.exec_()
+
+self.doubleSpinBox.setProperty("value", 10.0)  # PyQt4
+
+self.doubleSpinBox.setValue(10.0)  # PySide
+
+self.doubleSpinBox.setToolTip(_translate("MainWindow", "Coordinate placement Axis Y", None))  # PyQt4
+
+self.doubleSpinBox.setToolTip(_fromUtf8("Coordinate placement Axis Y"))  # PySide
+
+self.doubleSpinBox.setToolTip(u"Coordinate placement Axis Y.")# PySide
+
+import pivy
+
+from pivy import coin
+App.newDocument() # Open a document and a view 
+view = Gui.ActiveDocument.ActiveView 
+FCSceneGraph = view.getSceneGraph() # returns a pivy Python object that holds a SoSeparator, the main "container" of the Coin scenegraph
+FCSceneGraph.addChild(coin.SoCube()) # add a box to scene
+
+from lazy_loader.lazy_loader import LazyLoader
+Part = LazyLoader('Part', globals(), 'Part')
+
+P = LazyLoader('Part', globals(), 'Part')
+
+utils = LazyLoader('PathScripts', globals(), 'PathScripts.PathUtils')
