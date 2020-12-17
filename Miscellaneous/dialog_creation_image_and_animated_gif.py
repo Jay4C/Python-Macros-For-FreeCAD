ad7690132a354c3f9cb455054d88aca7ef892980
Jason ALOYAU <jason.aloyau@outlook.fr>
2020-12-17 13:28:24 +0100

    https://wiki.freecadweb.org/Dialog_creation_image_and_animated_GIF

---
+++ Miscellaneous/dialog_creation_image_and_animated_gif.py
@@ -0,0 +1,30 @@
+# https://wiki.freecadweb.org/Scripting_examples
+# https://wiki.freecadweb.org/Dialog_creation_image_and_animated_GIF
+
+import PySide
+from PySide import QtGui ,QtCore
+from PySide.QtGui import QPixmap, QMovie, QLabel
+from PySide.QtCore import *
+class MyLabelPatience():
+    label = QtGui.QLabel()
+    label.setText("<img src=" + path_Name_Image + "><b><center>Wait please</center> \n\n<center>i search the fonts !\n\n</center></b>")
+    # center screen
+    ecran = FreeCADGui.getMainWindow().frameGeometry()
+    xF = 250; yF = 120
+    xW = (ecran.width()/2) - (xF/2)
+    yW = (ecran.height()/2)- (yF/2)
+    label.setGeometry(xW, yW, xF, yF)
+    ####
+    label.setStyleSheet("QLabel {background-color : #F0C300;font: 12pt; }");
+    label.setWindowFlags(Qt.WindowFlags(Qt.FramelessWindowHint))        # pas de bords (not border)
+    ### un-comment for use ###############
+    movie = QtGui.QMovie(path_Name_Image)    # anime le fichier Gif anime (decommenter)
+    label.setMovie(movie)
+    movie.start()
+    ##################
+
+patience = MyLabelPatience().label
+patience.show()                    #show the image
+#patience.close()                   #close the Qlabel
+#MyLabelPatience().movie.start()    #start the animation (after patience.show())
+#MyLabelPatience().movie.stop()     #stop animation
