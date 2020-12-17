854e6b61a72c61cd079bc728778bcf1271f833e9
Jason ALOYAU <jason.aloyau@outlook.fr>
2020-12-17 13:30:15 +0100

    https://wiki.freecadweb.org/Command

---
+++ Miscellaneous/command.py
@@ -0,0 +1,29 @@
+# https://wiki.freecadweb.org/Scripting_examples
+# https://wiki.freecadweb.org/Command
+
+from PySide.QtCore import QT_TRANSLATE_NOOP
+
+class MyCommand:
+    """Explanation of the command."""
+
+    def __init__(self):
+        """Initialize variables for the command that must exist at all times."""
+        pass
+
+    def GetResources(self):
+        """Return a dictionary with data that will be used by the button or menu item."""
+        return {'Pixmap': 'MyCommand.svg',
+                'Accel': "Ctrl+A",
+                'MenuText': QT_TRANSLATE_NOOP("My_Command", "My Command"),
+                'ToolTip': QT_TRANSLATE_NOOP("My_Command", "Runs my command in the active document")}
+
+    def Activated(self):
+        """Run the following code when the command is activated (button press)."""
+        print("Activated")
+
+    def isActive(self):
+        """Return True when the command should be active or False when it should be disabled (greyed)."""
+        return True
+
+# The command must be "registered" with a unique name by calling its class.
+FreeCADGui.addCommand('My_Command', MyCommand())
