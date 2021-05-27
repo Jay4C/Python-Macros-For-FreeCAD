f7cab26f7fda4eb4a72a751959c7317789f0425f
Jason ALOYAU <jason.aloyau@outlook.fr>
2021-05-27 16:50:26 +0200

    Brevet_US_4661747_Edwin_Gray_Power_Tube

---
+++ Brevet_US_4661747_Edwin_Gray_Power_Tube/part_support.py
@@ -0,0 +1,96 @@
+import FreeCAD, Part, Drawing, math, ImportGui
+
+DOC = FreeCAD.activeDocument()
+
+DOC_NAME = "part_support"
+
+
+def clear_doc():
+    # Clear the active document deleting all the objects
+    for obj in DOC.Objects:
+        DOC.removeObject(obj.Name)
+
+
+def setview():
+    # Rearrange View
+    FreeCAD.Gui.SendMsgToActiveView("ViewFit")
+    FreeCAD.Gui.activeDocument().activeView().viewAxometric()
+
+
+if DOC is None:
+    FreeCAD.newDocument(DOC_NAME)
+    FreeCAD.setActiveDocument(DOC_NAME)
+    DOC = FreeCAD.activeDocument()
+else:
+    clear_doc()
+
+# EPS= tolerance to use to cut the parts
+EPS = 0.10
+EPS_C = EPS * (-0.5)
+
+# part_support
+part_support = Part.makeBox(53, 53, 5)
+
+# cylinder
+cylinder = Part.makeCylinder(2.5, 5)
+
+# part_support cut by cylinder for hole 1
+cylinder_vector = FreeCAD.Vector(5.5, 5.5, 0)
+cylinder.translate(cylinder_vector)
+part_support = part_support.cut(cylinder)
+
+# part_support cut by cylinder for hole 2
+cylinder_vector = FreeCAD.Vector(42, 0, 0)
+cylinder.translate(cylinder_vector)
+part_support = part_support.cut(cylinder)
+
+# part_support cut by cylinder for hole 3
+cylinder_vector = FreeCAD.Vector(0, 42, 0)
+cylinder.translate(cylinder_vector)
+part_support = part_support.cut(cylinder)
+
+# part_support cut by cylinder for hole 4
+cylinder_vector = FreeCAD.Vector(-42, 0, 0)
+cylinder.translate(cylinder_vector)
+part_support = part_support.cut(cylinder)
+
+# part_support cut by cylinder for hole 5 for supporting the electrode
+cylinder_vector = FreeCAD.Vector(21, -21, 0)
+cylinder.translate(cylinder_vector)
+part_support = part_support.cut(cylinder)
+
+# box_1
+box_1 = Part.makeBox(27, 27, 2)
+box_1_vector = FreeCAD.Vector(13, 13, 0)
+box_1.translate(box_1_vector)
+
+# part_support cut by box_1
+part_support = part_support.cut(box_1)
+
+# box_2
+box_2 = Part.makeBox(53, 53, 2)
+
+# box_3
+box_3 = Part.makeBox(29, 29, 2)
+box_3_vector = FreeCAD.Vector(12, 12, 0)
+box_3.translate(box_3_vector)
+
+# box_2 cut by box_3
+box_2 = box_2.cut(box_3)
+
+# part_support cut by box_2
+part_support = part_support.cut(box_2)
+
+Part.show(part_support)
+
+DOC.recompute()
+
+__objs__ = []
+
+__objs__.append(FreeCAD.getDocument("part_support").getObject("Shape"))
+
+step_file = u"path/to/part_support.step"
+
+ImportGui.export(__objs__, step_file)
+
+setview()
